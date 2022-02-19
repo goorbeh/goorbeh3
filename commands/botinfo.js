9f7c520fb07c4a17f5c80ba3cf99ad77e447df9d
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-19 17:28:50 +0330

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,13 +7,18 @@ module.exports = {
     category: "General",
 
     run: async(client, message, args) => {
+let count = 0;
+    client.guilds.cache.forEach((guild) => {
+        count += guild.memberCount
+    })
+
         let embed = new Discord.MessageEmbed()
         .setTitle("اطلاعات بات")
         .addField("سرور ها", `${client.guilds.cache.size} `)
         .addField("زبان ربات", "JS")
         .addField("اونر", "<@779956854720823296>")
         .addField("پینگ",`${client.ws.ping}MS`)
-        .addField("تعداد ممبر ها", `${client.users.cache.size}`,)
+        .addField("تعداد ممبر ها", `${count}`,)
         .setColor("YELLOW")
         .setFooter("GOORBEH bot")
         message.channel.send({embeds: [embed]})
