fe534fcba33aeeb645b86c4b657d9ca0fe817174
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-20 14:23:46 +0330

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -9,7 +9,7 @@ module.exports = {
   run: async(client, message, args) => {
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     if (!member) return message.channel.send("باوا این کیه دیگه")
-    if (member.id === message.author.id) return message.chanel.send("میخوای با خودت لب بگیری؟!")
+    if (member.id === message.author.id) return message.channel.send("میخوای با خودت لب بگیری؟!")
 const Response = Math.floor(Math.random() * kissgifs.length);
     let embed = new Discord.MessageEmbed()
     .setDescription(`جناب <@${message.author.id}> این <@${member.id}> را بوسید  :kiss:`)
