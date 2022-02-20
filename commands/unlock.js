4575d459cb31c3c5b8f65f39a9037e6a9edc7a2e
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-20 13:29:16 +0330

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!message.member.permissions.has('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
             return message.channel.send("Shoma Permission Nadarid 💩")
             }
-            message.channel.permissionOverwrites.edit(channel.guild.roles.everyone, { SEND_MESSAGES: true }).catch((e) => { console.error(e) })
+            message.channel.permissionOverwrites.edit(message.channel.guild.roles.everyone, { SEND_MESSAGES: true }).catch((e) => { console.error(e) })
             const embed = new Discord.MessageEmbed()
             .setTitle("Channel Update Shod")
             .setDescription(`🔓 ${message.channel}  Unlock Shod`)
