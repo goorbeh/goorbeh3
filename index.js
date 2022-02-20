cd4cf30b766d97362b1bd98ffad4308b8d7d4498
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-20 21:05:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -97,8 +97,6 @@ client.on("message", async message => {
 
   let command = client.commands.get(cmd);
 
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-
   if (command) command.run(client, message, args);
 })
 
