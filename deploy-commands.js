6e0f05988a7f16af309273523100d0feb3744ed5
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-23 13:58:26 +0330

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,17 +1,15 @@
 const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { token } = require('./botconfig.json')
 
 const { Routes } = require('discord-api-types/v9');
-
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
+for (const file of commandFiles) { 
+ const command = require(`./commands/${file}`);
+ commands.push(command.data.toJSON());}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
