64324f53c2bf19ff3154715a123bac2f5699e67a
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-23 13:47:30 +0330

    Update pings.js

--- commands/pings.js
+++ commands/pings.js
@@ -1,27 +1,11 @@
-const { MessageEmbed, CommandInteraction, Client } = require("discord.js")
+const Discord = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    name: "ping",
-    description: "برای نمایش پینگ",
-
-    /**
-     * 
-     * @param {Client} client 
-     * @param {CommandInteraction} interaction 
-     */
-
-    run: async (client, interaction) => {
-        await interaction.deferReply({
-            ephemeral: false
-        });
-        await interaction.editReply({ content: "در حال دریافت اطلاعات..." }).then(async () => {
-            const ping = Date.now() - interaction.createdAt;
-            const api_ping = client.ws.ping;
-
-            await interaction.editReply({
-                content: "`🏓`",
-                embeds: [new MessageEmbed().setAuthor(`Pong Piano`, client.user.displayAvatarURL({ dynamic: true })).setColor(client.embedColor).setFooter(` ${interaction.member.user.username}`, interaction.member.user.displayAvatarURL({ dynamic: true })).addFields([{ name: " سرعت پاسخ :", value: `\`\`\`ini\n  ${ping}ms  \n\`\`\``, inline: true }, { name: "سرعت api :", value: `\`\`\`ini\n  ${api_ping}ms \n\`\`\``, inline: true }]).setTimestamp()]
-            });
-        })
+    data: new SlashCommandBuilder
+    .setName('اسم کامند')
+    .setDescription('دیسکریپشن کامند'),
+    async execute(interaction) {
+        message.channel.send('hi')
     }
-}
+    }
