32428b1383b7aad66f2f22e986f565b3edb89ecf
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-02-23 14:14:06 +0330

    Update pings.js

--- commands/pings.js
+++ commands/pings.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder
+    data: new SlashCommandBuilder()
     .setName('اسم کامند')
     .setDescription('دیسکریپشن کامند'),
     async execute(interaction) {
