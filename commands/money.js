d3f2f7b5946accde02c893e7a4e11bcd78cce583
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-03-14 10:47:24 +0330

    Create money.js

---
+++ commands/money.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+const MoneySchema = require('../schema/MoneySchema')
+
+module.exports = {
+    name: "money",
+    description: "kf",
+    usage: "nfnd",
+    category: "fhu",
+
+    run: async(client, message, args) => {
+
+        let money = await MoneySchema.findOne({
+            usetID: message.author.id,
+        })
+
+        let bank = await MoneySchema.findOne({
+            userID: message.author.id,
+        })
+
+        let embed = new Discord.MessageEmbed()
+        .addField("پول های کیف پولتان", `${money}`)
+        .addField("پول های بانک شما", `${bank}`)
+        .setTitle("پروقایل شما")
+        .setColor("RANDOM")
+
+        message.channel.send({
+            embed: embed,
+        })
+    }
+}
