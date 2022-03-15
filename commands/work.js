00887319d61b9d6c2a589d2499002db713037769
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-03-15 12:04:41 +0330

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -14,7 +14,7 @@ module.exports = {
         .setColor("RANDOM")
 
        const user = await MoneySchema.find({
-           message.author.id,
+           userID: message.author.id,
 })
 const money = parseInt(user[0].money) + 300
 MoneySchema.findOneAndUpdate({ userID: message.author.id }, { money: money });
