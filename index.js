08fdf48799321b169e8cc4f44ad5652fb0651319
aryaamidi <86183154+goorbeh@users.noreply.github.com>
2022-08-10 12:44:52 +0430

    Update index.js

--- index.js
+++ index.js
@@ -194,9 +194,7 @@ msg.channel.bulkDelete(1, true)
 
       const data = await captchaSchema.findOne({
             Guild: interaction.guild.id,
-        }).catch(err => {
-return;
-})
+        })
 
 const button = interaction.customId
           if(button === "yes") {
